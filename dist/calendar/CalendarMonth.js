3c730cdd80667461f8ec59d20fe050c48f4caf69
Rob Masters <rob.masters@showtimeanalytics.com>
2018-03-16 09:44:31 +0000

    Fix showing selection when given date range is not from 00:00:00

--- dist/calendar/CalendarMonth.js
+++ dist/calendar/CalendarMonth.js
@@ -101,7 +101,7 @@ var CalendarMonth = _react2.default.createClass({
     if (!hideSelection && value && _moment2.default.isMoment(value) && value.isSame(d, 'day')) {
       isSelectedDate = true;
     } else if (!hideSelection && value && (0, _isMomentRange2.default)(value)) {
-      isInSelectedRange = value.contains(d);
+      isInSelectedRange = value.contains(d) || value.contains(d.endOf('day'));
 
       isSelectedRangeStart = value.start.isSame(d, 'day');
       isSelectedRangeEnd = value.end.isSame(d, 'day');
--- src/calendar/CalendarMonth.jsx
+++ src/calendar/CalendarMonth.jsx
@@ -57,7 +57,7 @@ const CalendarMonth = React.createClass({
     if (!hideSelection && value && moment.isMoment(value) && value.isSame(d, 'day')) {
       isSelectedDate = true;
     } else if (!hideSelection && value && isMomentRange(value)) {
-      isInSelectedRange = value.contains(d);
+      isInSelectedRange = (value.contains(d) || value.contains(d.endOf('day')));
 
       isSelectedRangeStart = value.start.isSame(d, 'day');
       isSelectedRangeEnd = value.end.isSame(d, 'day');
